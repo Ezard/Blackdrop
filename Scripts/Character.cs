73c7d065908e0c9d0567837543120241a2354c1e
Ezard <ezard.ben@gmail.com>
2014-12-06 14:26:14 +0000

    Added mini-level

---
+++ Scripts/Character.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+	int jumpCount = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.A)) {
+			transform.Translate(-0.015f, 0, 0);
+		}
+		if (Input.GetKey(KeyCode.D)) {
+			transform.Translate(0.015f, 0, 0);
+		}
+		if (Input.GetKeyDown(KeyCode.Space) && jumpCount < 2) {
+			rigidbody2D.velocity = new Vector2(0, 3);
+			jumpCount++;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.transform.position.y <= collider2D.bounds.min.y) {
+			jumpCount = 0;
+		}
+	}
+}
