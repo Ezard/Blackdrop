32ba96e8622a4fe32001342928023ca45867b964
Ezard <ezard.ben@gmail.com>
2014-12-06 19:25:25 +0000

    Broken block pieces will now fade out as they fall

--- CrumblingBlock.cs
+++ CrumblingBlock.cs
@@ -8,13 +8,19 @@ public class CrumblingBlock : MonoBehaviour {
 	void Start () {
 	
 	}
-	
+
+	float velocity = 0;
 	// Update is called once per frame
 	void Update () {
 		if (Time.time > crumbleTime && crumbleTime != 0) {
-			foreach(Transform child in transform) {
-				child.rigidbody2D.gravityScale = Random.value * 2f;
+			foreach (Transform child in transform) {
+				child.rigidbody2D.gravityScale = Random.value;
 				collider2D.enabled = false;
+			}
+			crumbleTime = 0;
+		} else if (crumbleTime == 0) {
+			foreach (Transform child in transform) {
+				child.renderer.material.color = new Color(1, 1, 1, Mathf.SmoothDamp(child.renderer.material.color.a, 0, ref velocity, 0.5f));
 			}
 		}
 	}
