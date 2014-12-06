c6c50d463039b233a2f9e1e28a733fc1a47a74ce
Ezard <ezard.ben@gmail.com>
2014-12-06 12:27:32 +0000

    Added scene with flickering light

---
+++ Scripts/LightFlicker.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightFlicker : MonoBehaviour {
+	public float nextOff = 0;
+	public float nextOn = 0;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > nextOff) {
+			nextOn = Time.time + 0.005f;
+			nextOff = float.MaxValue;
+			gameObject.light.enabled = false;
+		}
+		if (Time.time > nextOn) {
+			nextOff = Time.time + Random.value * 3;
+			nextOn = float.MaxValue;
+			gameObject.light.enabled = true;
+		}
+	}
+}
